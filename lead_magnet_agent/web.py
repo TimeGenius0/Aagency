30641457d3ec163c9e440481a095edb7cb9c1463
Bilel <Bilel.buraway@gmail.com>
2026-04-05 09:30:01 -0700

    feat(lead-magnet): scaffold package and web.py re-export

---
+++ lead_magnet_agent/web.py
@@ -0,0 +1,4 @@
+"""Re-export WebClient from linkedin_agent for use within lead_magnet_agent."""
+from linkedin_agent.web import WebClient
+
+__all__ = ["WebClient"]
---
+++ tests/test_lead_magnet_researcher.py
@@ -0,0 +1,5 @@
+from lead_magnet_agent.web import WebClient
+
+
+def test_webclient_importable():
+    assert WebClient is not None
